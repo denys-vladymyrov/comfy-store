83e8227eebf85bcbf4b0f03b63e7b28e52772ca0
Denys Vladymyrov <link_amd@yahoo.com>
2024-10-16 14:04:25 +0300

    updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
+import { useState } from "react";
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import { Navlinks } from "./index.js";
 
 const Navbar = () => {
+    const [theme, setTheme] = useState(false);
+    const handleTheme = () => {
+        setTheme(!theme);
+    };
+
     return (
         <nav className='bg-base-200'>
             <div className='navbar align-element '>
@@ -34,16 +40,16 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className='navbar-end'>
-                    {/* THEME ICONS */}
-                    {/* CART LINK*/}
-                    <NavLink to='cart' className='btn btn-ghost btn-circle btn-md ml-4'>
-                        <div className='indicator'>
-                            <BsCart3 className='h-6 w-6' />
-                            <span className='badge badge-sm badge-primary indicator-item'>
-                8
-              </span>
-                        </div>
-                    </NavLink>
+                    <label className='swap swap-rotate '>
+                        {/* this hidden checkbox controls the state */}
+                        <input type='checkbox' onChange={handleTheme} />
+
+                        {/* sun icon */}
+                        <BsSunFill className='swap-on h-4 w-4' />
+
+                        {/* moon icon */}
+                        <BsMoonFill className='swap-off h-4 w-4' />
+                    </label>
                 </div>
             </div>
         </nav>
